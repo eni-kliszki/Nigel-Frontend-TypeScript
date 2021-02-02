1507eb6aff08d591817fcd7f857d1f36c8838d31
eni-kliszki <kliszkieniko@gmail.com>
2021-02-02 19:58:41 +0100

    add navbar component with style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,24 @@
+import {Nav, LinkContainer, Link, RightSide, LeftSide} from './Navbar.styles';
 
 export const Navbar = () => {
+    const followers = "204.465.173";
+
     return (
-        <div>
-            
-        </div>
+        <Nav >
+            <RightSide>
+                <Link className='nigel-logo' href="#home">Nigel</Link>
+            </RightSide>
+            <LeftSide>
+                <LinkContainer className="links">
+                    <Link href="#features">Followers: {followers}</Link>
+                    <Link href="#about">Meet Nigel</Link>
+                    <Link href="#registration">Sign Up</Link>
+                </LinkContainer>
+                <form >
+                    <input type="text" placeholder="Search" className="search-on-page" />
+                    <button >Search</button>
+                </form>
+            </LeftSide>
+        </Nav>
     )
 }
