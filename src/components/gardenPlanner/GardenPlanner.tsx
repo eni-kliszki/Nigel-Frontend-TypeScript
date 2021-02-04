5739cedb3c4fa17c177fc59bc89107842b7db8eb
Adam Sallai <yadam1998@gmail.com>
2021-02-04 14:24:52 +0100

    add the 3 components to GardenPlanner

--- src/components/gardenPlanner/GardenPlanner.tsx
+++ src/components/gardenPlanner/GardenPlanner.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { GardenPlannerPage } from './styled-components/GardenPlanner.styles';
+import Garden from './garden/Garden';
+import Sidebar from './sidebar/Sidebar';
+import PlantContainer from './footer/PlantContainer';
 
 const GardenPlanner = () => {
   return (
     <GardenPlannerPage>
-      
+      <Garden />
+      <Sidebar />
+      <PlantContainer />
     </GardenPlannerPage>
   )
 }
