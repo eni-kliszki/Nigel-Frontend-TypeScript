ab95a3f85c23d00bcd3ae2675985443126432342
MMaGH <misk.mardi@gmail.com>
2021-02-04 14:20:14 +0100

    restructure folders, add ConveyorStyles.ts

---
+++ src/styled_components/ConveyorStyles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ConveyorContainer = styled.div`
+
+`
+
+export const ArrowButton = styled.div`
+
+`
+
+export const ConveyorItem = styled.div`
+
+`
